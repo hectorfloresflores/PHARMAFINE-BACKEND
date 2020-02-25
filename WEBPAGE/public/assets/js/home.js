709ebb78d87a0b0d78fc74979876a4ca8b922cd9
hectorfloresflores <48966777+hectorfloresflores@users.noreply.github.com>
2020-02-24 22:26:23 -0600

    v13
    Added handlebar to the home page

---
+++ WEBPAGE/public/assets/js/home.js
@@ -0,0 +1,77 @@
+
+"use strict";
+
+
+document.addEventListener("click", function(event) {
+  let userEmail = document.getElementById("ph_user_email");
+  let userMessage = document.getElementById("ph_user_message");
+  let emailWarning = document.getElementById("ph_email_warning");
+  let xhr = new XMLHttpRequest();
+  switch (event.target.id) {
+        case "ph_button_email":
+          xhr.open("POST", "http://localhost:3002/email");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(
+          JSON.stringify({
+            email: userEmail.value,
+            message: userMessage.value
+          })
+        );
+      
+        xhr.onload = function() {
+          switch (xhr.status) {
+            case 400:
+              emailWarning.innerHTML = `
+                          <div class="alert alert-danger alert-dismissible fade show">
+                              <strong>Lo sentimos,</strong>
+                              el mensaje no se pudo enviar
+                          </div>
+                          `;
+              console.log(xhr.responseText);
+      
+              break;
+      
+            case 200:
+              emailWarning.innerHTML = `
+                          <div class="alert alert-success alert-dismissible fade show">
+                              <strong>Mensaje enviado con éxito</strong>
+                              Nos pondremos en contacto de inmediato contigo.
+                          </div>
+                          `;
+              console.log(xhr.responseText);
+              break;
+      
+            default:
+              console.log(`${xhr.status} : ${xhr.statusText}`);
+      
+              break;
+          }
+        };
+          break;
+          default:
+          break;
+  }
+})
+
+function checkValidityEmail() {
+  // alert()
+  // let buttonEmail = document.getElementById("ph_button_email");
+  // if ( document.querySelector('#ph_user_email').checkValidity() == true &&
+  // document.querySelector('#ph_user_message').checkValidity() == true) {
+  //   buttonEmail.removeAttribute("disabled");
+  // }else{
+  //   buttonEmail.setAttribute("disabled", "");
+  // }
+   
+}
+
+
+/*Refresh page when close modal email */
+$("#ph_modal_email").on("hidden.bs.modal", function() {
+  window.location.reload();
+});
+
+
+
+ 
+
--- WEBPAGE/server.js
+++ WEBPAGE/server.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/*To use routes and server mode */
+/**
+ * Imports
+ */
+// For create server
 const express = require('express');
+// For access data from one file to another
 const cors = require('cors');
-/**/
+// For render hbs files
+const exphbs  = require('express-handlebars');
+// For get the path
+const path = require('path');
 
-// const cors = require('cors');
+/**
+ * Uses of imports
+ */
 const app = express();
 /*Users Mongo DB functions Schema */
 const {User, findUsersBy,updateUser} = require('./db/users');
@@ -13,16 +22,28 @@ const {User, findUsersBy,updateUser} = require('./db/users');
 const port = 3002;
 
 
-/*App uses */
-
+/**
+ * App uses
+ */
+// To parse json data and string
 app.use(express.json())
+// To access data from one file to another
 app.use(cors())
+// Routing
 app.use('/',require('./routes/users'));
 app.use('/',require('./routes/checkout'));
 app.use('/',require('./routes/email'));
 app.use('/',require('./routes/openpay'));
 app.use('/',require('./routes/emailConfirmation'));
-app.use(express.static(__dirname +"/public"))
+// Static routing
+app.use(express.static(path.join(__dirname, '/public')));
+// To use handlebars
+app.engine('hbs', exphbs({ 
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, './views/layouts/') 
+}));
+app.set('view engine', 'hbs');
 /***********/
 
 
@@ -34,51 +55,10 @@ function myMiddleware(req,res, next){
 
 
 
-app.get('/home', myMiddleware, (req,res)=>{
-    res.send('Hello !')
+app.get('/home1', (req,res)=>{
+    res.render('home.hbs')
 })
 
-
-
-app.get('/us', async (req,res)=>{
-    let users = await getUsersA()
-    res.send(users)
-})
-
-// app.route('/productos')
-//     .get((req,res)=>{
-//         let id = req.query.id
-//         let marca = req.query.marca
-//         console.log(id, marca);
-        
-//         let rs ="Params: "
-//         rs += (id)? "id "+id: rs;
-//         rs += (marca)? "marca "+marca: rs;
-
-//         console.log("get products");
-//         res.send(rs)
-//     })
-//     .post((req,res)=>{
-//         console.log("post products ")
-//         console.log(req.body);
-//         res.status(418).send("error")
-//     })
-//     .delete((req,res)=>{
-//         console.log("delete products")
-//         res.status(400).send("borrado")
-//     });
-
-// app.route('/productos/:uid')
-//     .get((req,res)=>{
-//         if(req.get('x-auth')!=undefined){
-//             let id = req.params.uid;
-//             res.set('x-id',id)
-//             res.send({nombre:"mouse",marca:"MS"}) 
-//         }else{
-//             res.status(400).send({error: 'falta x-auth'})
-//         }
-       
-//     })
-
+/**Port of app to listen */
 app.listen(port, ()=> console.log(`http://localhost:${port}`))
 
